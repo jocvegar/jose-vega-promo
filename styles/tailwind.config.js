c92fad21c3f9796840a8e374a321d199e810049c
Jose <jocvegar@gmail.com>
2020-07-16 11:21:24 -0600

    Adds starter code

---
+++ styles/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      fontFamily: {
+        'sans': ['Noto Sans KR', 'Sans-serif']
+      },
+      extend: {
+        colors: {
+          'regal-blue': '#243c5a',
+        }
+      }
+    },
+    variants: {},
+    plugins: [],
+  }
