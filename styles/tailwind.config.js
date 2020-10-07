32be7e84faaeb78bd944fcece69153a77807f808
Jose <jocvegar@gmail.com>
2020-10-07 17:04:38 -0600

    Adds examples

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,15 +1,15 @@
 // tailwind.config.js
 module.exports = {
-    theme: {
-      fontFamily: {
-        'sans': ['Noto Sans KR', 'Sans-serif']
-      },
-      extend: {
-        colors: {
-          'regal-blue': '#243c5a',
-        }
-      }
+  theme: {
+    fontFamily: {
+      'sans': ['Source Sans Pro', 'Sans-serif']
     },
-    variants: {},
-    plugins: [],
-  }
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
